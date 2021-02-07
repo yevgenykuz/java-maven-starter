24b40af19064155d73de2f5391222d429edf4023
Yevgeny Kuznetsov <yevgenyku@gmail.com>
2021-02-06 22:45:50 -0500

    Update group ID

--- src/main/java/com/yevgenyk/Main.java
+++ src/main/java/com/yevgenyk/Main.java
@@ -1,4 +1,4 @@
-package me.yevgeny;
+package com.yevgenyk;
 
 import java.io.IOException;
 import java.io.InputStream;
--- src/test/java/com/yevgenyk/MainTest.java
+++ src/test/java/com/yevgenyk/MainTest.java
@@ -1,4 +1,4 @@
-package me.yevgeny;
+package com.yevgenyk;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
